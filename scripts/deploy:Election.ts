863b7b7e3dfd2585e58bfb571bddd677632eab34
Gading Nasution <sutan.gnst@gmail.com>
2023-01-03 19:19:14 +0700

    log signer on deploy election script

--- scripts/deploy:Election.ts
+++ scripts/deploy:Election.ts
@@ -1,6 +1,10 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log('Deploying contracts with the account:', deployer.address);
+  console.log('Account balance:', (await deployer.getBalance()).toString());
+
   const Election = await ethers.getContractFactory("Election");
   const election = await Election.deploy();
 
