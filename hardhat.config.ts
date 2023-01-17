13588147fc967ef02d0ff17a4946b800c5c095ce
Gading Nasution <sutan.gnst@gmail.com>
2023-01-17 09:13:50 +0700

    setup env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import DotEnv from 'dotenv';
+
+DotEnv.config({ path: '.env' });
+
+const {
+  GOERLI_ACCOUNT_PRIVATE_KEY = '',
+  SANDBOX_2P5_URL = '',
+  ALCHEMY_KEY = ''
+} = process.env;
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -9,18 +18,22 @@ const config: HardhatUserConfig = {
       },
     ],   
   }, 
-  defaultNetwork: "ganache",
+  defaultNetwork: "sandbox2p5",
   networks: {
+    sandbox2p5: {
+      url: SANDBOX_2P5_URL,
+      chainId: 2525
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+      accounts: [GOERLI_ACCOUNT_PRIVATE_KEY],
+      gasPrice: 20000000000,
+    },
     ganache: {
       url: "http://127.0.0.1:7545",
       // gasLimit: 6000000000,
       // defaultBalanceEther: 10,
-    },
-    // goerli: {
-    //   url: "https://eth-goerli.g.alchemy.com/v2/ALCHEMY-KEY",
-    //   accounts: ["PRIVATE-KEY"],
-    //   gasPrice: 20000000000,
-    // }
+    }
   }
 };
 
--- scripts/deploy:Election.ts
+++ scripts/deploy:Election.ts
@@ -1,16 +1,19 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
-  console.log('Deploying contracts with the account:', deployer.address);
-  console.log('Account balance:', (await deployer.getBalance()).toString());
+  try {
+    const [deployer] = await ethers.getSigners();
+    console.log('Deploying contracts with the account:', deployer.address);
+    console.log('Account balance:', (await deployer.getBalance()).toString());
 
-  const Election = await ethers.getContractFactory("Election");
-  const election = await Election.deploy();
+    const Election = await ethers.getContractFactory("Election");
+    const election = await Election.deploy();
 
-  await election.deployed();
-
-  console.log(`Election contract address: ${election.address}`);
+    await election.deployed();
+    console.log(`Election contract address: ${election.address}`);
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
